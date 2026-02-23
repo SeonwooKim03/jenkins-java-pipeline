c7eb4e3abbbd221bb86617ab0bf84f16540004f0
SeonwooKim03 <seonwook@mtu.edu>
2026-02-23 00:39:50 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
-public class Hi {
+public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins!");
+        String s = null;
+        System.out.println(s.length());
     }
 }
